8b925cc4e2dcb4239287f5eade30af9c310e4425
Garret29 <szymon.piotrowski93@gmail.com>
2017-01-16 14:13:28 +0100

    initial commit II

--- src/main/java/pl/dtree/pdf_invoice_generator/Main.java
+++ src/main/java/pl/dtree/pdf_invoice_generator/Main.java
@@ -8,23 +8,6 @@ import java.awt.event.ActionListener;
 public class Main {
 
     public static void main(String s[]) {
-        JFrame frame = new JFrame();
-        final JFrame frame1 = new JFrame();
-        final JFileChooser chooser = new JFileChooser();
-        JButton button = new JButton("zapisz");
-        frame1.add(chooser);
-
-        ActionListener actionListener = new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame1.setVisible(true);
-            }
-        };
-
-        button.addActionListener(actionListener);
-
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.add(button);
-        frame.setVisible(true);
 
     }
 }
